75ceb856e4e3b28886ab950efdcb454d266b861d
Frederik Larsen <frederikbontved@gmail.com>
2020-01-26 21:55:54 +0100

    add unstyled slider

--- src/Palette.js
+++ src/Palette.js
@@ -3,17 +3,37 @@ import ColorBox from './ColorBox'
 import './Palette.css'
 import SeedColors from './seedColors';
 import { generatePalette } from './colorHelpers'
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
 
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {level: 500};
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({level});
+    }
     render() {
-        console.log(generatePalette(SeedColors[4]));
+        const { colors } = this.props.palette;
+        const { level } = this.state;
 
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name} />
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />
         ));
 
         return (
             <div className="Palette">
+
+                <Slider 
+                    defaultValue={level} 
+                    min={100}
+                    max={900} 
+                    step={100}
+                    onAfterChange={this.changeLevel} 
+                />
+
                 { /* navbar goes here */ }
                 <div className="Palette-colors">
                 {colorBoxes}

b1a17c1eb8ce7c275ac696046ec7a38acfc0cb39
Frederik Larsen <frederikbontved@gmail.com>
2020-01-28 23:06:28 +0100

    move out styles to seperate files

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,51 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import ColorBox from './ColorBox'
 import Navbar from './Navbar'
-import PaletteFooter from './PaletteFooter';
+import PaletteFooter from './PaletteFooter'
+import styles from './styles/PaletteStyles'
 import { withStyles } from '@material-ui/styles'
 
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-    },
-    paletteColors: {
-        height: "90vh"
-    },
-    goBack: {
-        width: "20%",
-        height: "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-        opacity: "1",
-        backgroundColor: "black",
-
-        "& a": {
-            width: "100px",
-            height: "30px",
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(255, 255, 255, .3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            color: "white",
-            textTransform: "uppercase",
-            border: "none",
-            transition: ".5s",
-            textDecoration: "none"
-        }
-    }
-}
+import ColorBox from './ColorBox'
 
 class SingleColorPalette extends Component {
     constructor(props) {
